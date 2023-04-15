1595fb0efbba1ac4c2cf55e0e6367e9fee4d3c5f
MaximManuyko <manuyko.m@gmail.com>
2023-04-15 11:27:35 +0300

    Add PercentageCalculator

---
+++ percentageCalculator.js
@@ -0,0 +1,6 @@
+const percentageCalculator = (number, total) => {
+   return number * 100 / total;
+};
+
+console.log('на сколько процентов прогрузился мецяц апрель')
+console.log(percentageCalculator(6, 30));
